a49aeb94fb9d0921b21284db8aa693a38a6c0052
LuukBlankenstijn <luukblankenstijn@gmail.com>
2023-10-16 20:05:02 +0200

    Implemented first stage

--- src/main/java/ned/dejonghblankenstijn/FrameBuilder.java
+++ src/main/java/ned/dejonghblankenstijn/FrameBuilder.java
@@ -7,20 +7,25 @@ import java.awt.event.ActionListener;
 
 
 public class FrameBuilder implements ActionListener {
-    protected boolean moveChar = true;
-    protected boolean moveCloud = true;
-    protected boolean drawStartText = true;
-
-
-
+    protected DrawPlane imagePlane = new DrawPlane();
 
     private final Clouds clouds = new Clouds();
     private final Track track = new Track();
     private final MovingCharacter character = new MovingCharacter();
 
+    private boolean moveChar;
+    private boolean moveCloud;
+    private boolean drawStartText;
+    private boolean stageOne;
 
-    protected DrawPlane imagePlane = new DrawPlane();
+    protected final Timer timer = new Timer(100, this);
+    private long timerCycle = 0;
 
+//  Stage One variables
+    protected int seconds;
+    private String message = "Click the following key as fast as possible:";
+
+    private char key = 51;
 
     protected class DrawPlane extends JPanel {
         public void paintComponent(Graphics g) {
@@ -28,40 +33,44 @@ public class FrameBuilder implements ActionListener {
             g.drawImage(track.showImage,0,368,this);
             g.drawImage(character.showImage, 350, 500, this );
             if (drawStartText) {
+                g.setFont(new Font("Monaco", Font.PLAIN,80));
                 g.drawString("Press space to start!",100, 300);
             }
+            if (stageOne) {
+                g.setFont(new Font("Monaco", Font.PLAIN, 40));
+                g.drawString(message,100,300);
+                g.setFont(new Font("Monaco", Font.PLAIN, 60));
+                g.drawString("" + key, 450, 400);
+            }
         }
+
     }
-
-    public FrameBuilder() {
-        imagePlane.setFont(new Font("Monaco", Font.PLAIN, 80));
-    }
-
-    final Timer timer = new Timer(100, this);
-    int timerCycle = 0;
-
-    public void startTimer(){
-        timer.start();
-    }
-
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (timer.getDelay() > 100) {
-            timer.setDelay(timer.getDelay()-20);
+            timer.setDelay(Math.max(100, timer.getDelay()-20));
         }
          if (moveChar) {
              moveCharacter();
          }
          if (moveCloud) {
              if (timerCycle % 6 == 0) {
-                 timerCycle = 0;
+
                  moveClouds();
              }
          }
 
         timerCycle++;
         imagePlane.repaint();
+    }
+
+    private void setFontSize(int size) {
+        imagePlane.setFont(new Font("Monaco", Font.PLAIN, size));
+    }
+
+    public void startTimer(){
+        timer.start();
     }
 
     private void moveCharacter() {
@@ -75,4 +84,34 @@ public class FrameBuilder implements ActionListener {
 //    private void moveTrack() {
 //        track.nextImage();
 //    }
+
+
+
+    public char getKey() {
+        return key;
+    }
+
+    public boolean isStageOne() {
+        return stageOne;
+    }
+
+    public void setMoveChar(boolean moveChar) {
+        this.moveChar = moveChar;
+    }
+
+    public void setMoveCloud(boolean moveCloud) {
+        this.moveCloud = moveCloud;
+    }
+
+    public void setDrawStartText(boolean drawStartText) {
+        this.drawStartText = drawStartText;
+    }
+
+    public void setStageOne(boolean stageOne) {
+        this.stageOne = stageOne;
+    }
+
+    public void setKey(char countDown) {
+        this.key = countDown;
+    }
 }
